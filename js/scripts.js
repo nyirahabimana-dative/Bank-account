040084bf0a5291e60ac8f991c5ffd24d40da7166
nyirahabimana dative <nyiradative052@gmail.com>
2019-01-19 11:51:18 +0200

    add prototype

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@ function Balance(name,initialdeposit) {
     this.name = name;
     this.initialdeposit = initialdeposit;
   }
+
+   Balance.prototype.withdraw= function(amount) {
+    this.withdraw=this.balance-amount
+}
+Balance.prototype.deposit=function(amount){
+    this.deposit=this.balance+amount
+}
