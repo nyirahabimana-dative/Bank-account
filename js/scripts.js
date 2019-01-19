f00ec9076b8d2d5f21d6ca8dd77d682d4b1d9d0d
nyirahabimana dative <nyiradative052@gmail.com>
2019-01-19 11:57:23 +0200

    add new changes

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,36 @@ function Balance(name,initialdeposit) {
 Balance.prototype.deposit=function(amount){
     this.deposit=this.balance+amount
 }
+var BankAccount = {
+    balance: 0,
+    withdraw: function(amount) {
+       this.balance = this.balance - amount;
+    },
+    deposit: function(amount) {
+       this.balance = this.balance + amount;
+    }
+    };
+$(document).ready(function() {
+    var newAccount = Object.create(BankAccount);
+    $("form#transactions").submit(function(event) {
+    event.preventDefault();
+    var inputtedName = $("input#name").val();
+    var inputtedBalance = parseInt($("input#initial").val());
+    newAccount.name = inputtedName;
+    newAccount.deposit(inputtedBalance);
+    $(".balance").text(newAccount.balance);
+    });
+    $("form#manage").submit(function(event) {
+    event.preventDefault();
+    var newBalance = parseInt($("input#deposit").val());
+    newAccount.deposit(newBalance);
+    $(".balance").text(newAccount.balance);
+
+    });
+    $("form#withdraw").submit(function(event) {
+    event.preventDefault();
+    var newBalance = parseInt($("input#withdraw").val());
+    newAccount.withdraw(newBalance);
+    $(".balance").text(newAccount.balance);
+    });
+    });
